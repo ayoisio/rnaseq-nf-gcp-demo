3c2a75846def7f3f07f8c78c0340ae55eefc401b
ayoisio <ayois.io>
2022-03-05 14:07:32 -0800

    Adding files for GSE25133 retrieval - 03/05/2022

--- GSE25133_raw_fastq_wget.sh
+++ GSE25133_raw_fastq_wget.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-while read line; do wget $line; done < GSE25133_raw_fastq_hg19_links.txt
+while read line; do wget $line; done < $HOME/rnaseq_gcp_nf_demo/GSE25133_raw_fastq_hg19_links.txt
